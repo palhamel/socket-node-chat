cd6a1869aac793e38ebfc4588d07b0556b202a4f
Pål Hamel <pal.hamel@gmail.com>
2021-03-17 20:33:59 +0100

    add read input and send

--- public/script.js
+++ public/script.js
@@ -1,10 +1,17 @@
-// init sockeIO
+// init socketIO
 const socket = io()
 
 // prevent page reload:
 const chat = document.querySelector('.chat-form')
+// read input value and send via socketIO
+const input = document.querySelector('.chat-input')
 
+// chat.addEventListener('submit', event => {
+//   event.preventDefault()
+// })
 chat.addEventListener('submit', event => {
   event.preventDefault()
+  socket.emit('chat', input.value) // sending data from the client
+  input.value = ''
 })
 
