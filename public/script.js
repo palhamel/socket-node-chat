99477013aa8b748d66762201ceb61e820b6adfad
Pål Hamel <pal.hamel@gmail.com>
2021-03-17 21:05:01 +0100

    render out message in clients div

--- public/script.js
+++ public/script.js
@@ -15,7 +15,17 @@ chat.addEventListener('submit', event => {
   input.value = ''
 })
 
+const chatWindow = document.querySelector('.chat-window')
+
+const renderMessage = message => {
+  const div = document.createElement('div')
+  div.classList.add('render-message')
+  div.innerText = message
+  chatWindow.appendChild(div)
+}
+
 socket.on('chat', message => {
   console.log('From server:', message)
+  renderMessage(message)
 })
 
